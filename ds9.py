60f3e2830dbbfb49e14253feff93063ebb2b6b73
Eric Mandel <eric@cfa.harvard.edu>
2014-11-18 14:12:43 -0500

    fix typo

--- ds9.py
+++ ds9.py
@@ -2,6 +2,6 @@ from pyds9 import *
 from warnings import warn as _warn
 
 _warn('You are importing pyds9 via the `ds9` package.  This package is now  '
-      'deprecated in favor of `pyds9`.  In the future you should usd '
+      'deprecated in favor of `pyds9`.  In the future you should use '
       '``import pyds9`` instead of ``import ds9``.  ``ds9`` will continue to '
       'work, but may be removed in the future.')
