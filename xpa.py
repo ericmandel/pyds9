12ab97cc0cee437e709c4bcefdc9724338522ead
Francesco Montesano <franz.bergesund@gmail.com>
2015-09-07 08:53:13 +0200

    Some PEP8 fix

--- xpa.py
+++ xpa.py
@@ -8,9 +8,10 @@ import sys
 import ctypes
 import ctypes.util
 
+
 # look for the shared library in sys.path
 def _find_shlib(_libbase):
-    _ulist=platform.uname()
+    _ulist = platform.uname()
     if _ulist[0] == 'Darwin':
         _libname = 'lib' + _libbase + '.dylib'
     elif ((_ulist[0] == 'Windows') or ((_ulist[0]).find('CYGWIN') != -1)):
@@ -27,25 +28,27 @@ def _find_shlib(_libbase):
         return _fname
     return None
 
-_libpath=_find_shlib('xpa')
+_libpath = _find_shlib('xpa')
 if _libpath:
-    libxpa=ctypes.cdll.LoadLibrary(_libpath)
-    _ulist=platform.uname()
+    libxpa = ctypes.cdll.LoadLibrary(_libpath)
+    _ulist = platform.uname()
     if ((_ulist[0] == 'Windows') or ((_ulist[0]).find('CYGWIN') != -1)):
-        libc=ctypes.cdll.msvcrt
+        libc = ctypes.cdll.msvcrt
     else:
-        libc=ctypes.cdll.LoadLibrary(None)
+        libc = ctypes.cdll.LoadLibrary(None)
 else:
     raise ImportError("can't find XPA shared library")
 
 # factory routine returning pointer to byte array
 c_byte_p = ctypes.POINTER(ctypes.c_byte)
 
+
 # free C buffers returned by xpa calls
-def _freebufs(p_arr, len):
-    for i in range(len):
+def _freebufs(p_arr, len_):
+    for i in range(len_):
         if p_arr[i]:
             libc.free(p_arr[i])
+
 
 ## XPA XPAOpen(char *mode);
 libxpa.XPAOpen.restype = ctypes.c_void_p
@@ -59,17 +62,19 @@ libxpa.XPAClose.argtypes = [ctypes.c_void_p]
 def XPAClose(xpa):
     libxpa.XPAClose(xpa)
 
+
 ## int XPAGet(XPA xpa, char *template, char *paramlist, char *mode,
 ##            char **bufs, int *lens, char **names, char **messages, int n);
 libxpa.XPAGet.restype = ctypes.c_int
 def XPAGet(xpa, target, paramlist, mode, bufs, lens, names, messages, n):
-    libxpa.XPAGet.argtypes = [ctypes.c_void_p, ctypes.c_char_p,     \
-                              ctypes.c_char_p, ctypes.c_char_p,     \
-                              c_byte_p*n, ctypes.c_int*n, 	    \
-                              c_byte_p*n, c_byte_p*n, 		    \
+    libxpa.XPAGet.argtypes = [ctypes.c_void_p, ctypes.c_char_p,
+                              ctypes.c_char_p, ctypes.c_char_p,
+                              c_byte_p*n, ctypes.c_int*n,
+                              c_byte_p*n, c_byte_p*n,
                               ctypes.c_int]
-    return libxpa.XPAGet(xpa, target, paramlist, mode,		    \
+    return libxpa.XPAGet(xpa, target, paramlist, mode,
                          bufs, lens, names, messages, n)
+
 
 ## int XPASet(XPA xpa,
 ##             char *template, char *paramlist, char *mode,
@@ -78,13 +83,14 @@ def XPAGet(xpa, target, paramlist, mode, bufs, lens, names, messages, n):
 XPASet = libxpa.XPASet
 libxpa.XPASet.restype = ctypes.c_int
 def XPASet(xpa, target, paramlist, mode, buf, blen, names, messages, n):
-    libxpa.XPASet.argtypes = [ctypes.c_void_p, ctypes.c_char_p,     \
-                              ctypes.c_char_p, ctypes.c_char_p,     \
-                              ctypes.c_char_p, ctypes.c_int,        \
-                              c_byte_p*n, c_byte_p*n,   	    \
+    libxpa.XPASet.argtypes = [ctypes.c_void_p, ctypes.c_char_p,
+                              ctypes.c_char_p, ctypes.c_char_p,
+                              ctypes.c_char_p, ctypes.c_int,
+                              c_byte_p*n, c_byte_p*n,
                               ctypes.c_int]
-    return libxpa.XPASet(xpa, target, paramlist, mode,		    \
+    return libxpa.XPASet(xpa, target, paramlist, mode,
                          buf, blen, names, messages, n)
+
 
 ## int XPAInfo(XPA xpa,
 ##              char *template, char *paramlist, char *mode,
@@ -92,25 +98,27 @@ def XPASet(xpa, target, paramlist, mode, buf, blen, names, messages, n):
 XPAInfo = libxpa.XPAInfo
 libxpa.XPAInfo.restype = ctypes.c_int
 def XPAInfo(xpa, target, paramlist, mode, names, messages, n):
-    libxpa.XPAInfo.argtypes = [ctypes.c_void_p, ctypes.c_char_p,    \
-                               ctypes.c_char_p, ctypes.c_char_p,    \
-                               c_byte_p*n, c_byte_p*n, \
+    libxpa.XPAInfo.argtypes = [ctypes.c_void_p, ctypes.c_char_p,
+                               ctypes.c_char_p, ctypes.c_char_p,
+                               c_byte_p*n, c_byte_p*n,
                                ctypes.c_int]
     return libxpa.XPAInfo(xpa, target, paramlist, mode, names, messages, n)
+
 
 ## int XPAAccess(XPA xpa,
 ##              char *template, char *paramlist, char *mode,
 ##              char **names, char **messages, int n);
 libxpa.XPAAccess.restype = ctypes.c_int
 def XPAAccess(xpa, target, paramlist, mode, names, messages, n):
-    libxpa.XPAAccess.argtypes = [ctypes.c_void_p, ctypes.c_char_p,  \
-                                 ctypes.c_char_p, ctypes.c_char_p,  \
-                                 c_byte_p*n, c_byte_p*n, 	    \
+    libxpa.XPAAccess.argtypes = [ctypes.c_void_p, ctypes.c_char_p,
+                                 ctypes.c_char_p, ctypes.c_char_p,
+                                 c_byte_p*n, c_byte_p*n,
                                  ctypes.c_int]
     return libxpa.XPAAccess(xpa, target, paramlist, mode, names, messages, n)
 
 # default value for n (max number of access points)
 xpa_n = 1024
+
 
 def xpaget(target, plist=None, n=xpa_n):
     buf_t = c_byte_p*n
@@ -125,10 +133,11 @@ def xpaget(target, plist=None, n=xpa_n):
         buf = []
         for i in range(got):
             if lens[i]:
-                cur=ctypes.string_at(bufs[i], lens[i])
+                cur = ctypes.string_at(bufs[i], lens[i])
                 buf.append(cur)
         for i in range(got):
-            if errs[i]: errmsg += ctypes.string_at(errs[i]).strip() + '\n'
+            if errs[i]:
+                errmsg += ctypes.string_at(errs[i]).strip() + '\n'
     else:
         buf = None
     _freebufs(bufs, n)
@@ -164,6 +173,7 @@ def xpaset(target, plist=None, buf=None, blen=-1, n=xpa_n):
         raise ValueError(errmsg)
     return got
 
+
 def xpainfo(target, plist=None, n=xpa_n):
     buf_t = c_byte_p*n
     names = buf_t()
@@ -178,6 +188,7 @@ def xpainfo(target, plist=None, n=xpa_n):
     if errmsg:
         raise ValueError(errmsg)
     return got
+
 
 def xpaaccess(target, plist=None, n=xpa_n):
     buf_t = c_byte_p*n
