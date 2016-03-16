619fb297c60d170d1d267cbedc20247864bc5fc9
Francesco Montesano <franz.bergesund@gmail.com>
2016-03-16 08:55:29 +0100

    xpans compile and doesn't crash

--- pyds9/setup_package.py
+++ pyds9/setup_package.py
@@ -1,9 +1,11 @@
 # Licensed under a 3-clause BSD style license - see PYFITS.rst
-from __future__ import absolute_import
+from __future__ import (print_function, absolute_import, division,
+                        unicode_literals)
 
 import os
 import platform
 from pprint import pprint
+import subprocess as sp
 import struct
 
 from distutils.core import Extension
@@ -15,6 +17,7 @@ from astropy_helpers.distutils_helpers import get_distutils_build_option
 def get_extensions():
     ulist = platform.uname()
     xpa_dir = os.path.join('cextern', 'xpa')
+    debug = get_distutils_build_option('debug')
 
     # libxpa configurations
     cfg = setup_helpers.DistutilsExtensionArgs()
@@ -22,7 +25,8 @@ def get_extensions():
 
     if 'CFLAGS' not in os.environ and struct.calcsize("P") == 4:
         if ulist[0] == 'Darwin' or ulist[4] == 'x86_64':
-            os.system('echo "adding -m32 to compiler flags ..."')
+            if debug:
+                print('adding -m32 to compiler flags ...')
             cflags = '-m32'
             cfg['extra_compile_args'].append(cflags)
 
@@ -33,7 +37,7 @@ def get_extensions():
     # import pdb; pdb.set_trace()
 
     if not setup_helpers.use_system_library('libxpa'):
-        if not get_distutils_build_option('debug'):
+        if not debug:
             # All of these switches are to silence warnings from compiling
             cfg['extra_compile_args'].extend([
                 '-Wno-declaration-after-statement',
@@ -68,10 +72,34 @@ def get_external_libraries():
     return ['libxpa']
 
 
-def pre_build_hook(cmd):
-    "test of the pre_build_hook"
-    pprint(cmd.__dict__)
+def post_build_ext_hook(cmd):
+    "Build the xpans executable"
+    compiler = cmd.compiler
+    libxpa = cmd.ext_map['pyds9.libxpa']
+    flags = libxpa.extra_compile_args
+    include_dirs = libxpa.include_dirs
+    file_name = libxpa._file_name
+    build_lib = cmd.build_lib
+    build_temp = cmd.build_temp
+
+    # use distutils compiler to build the xpans.o
+    xpans_c = os.path.join('cextern', 'xpa', 'xpans.c')
+    compiler.compile([xpans_c, ], output_dir=build_temp,
+                     include_dirs=include_dirs,
+                     debug=get_distutils_build_option('debug'),
+                     extra_postargs=flags, depends=[xpans_c, ])
     import pdb; pdb.set_trace()
+    # compile the executable by hand
+    xpans_o = os.path.join(build_temp, xpans_c.replace('.c', '.o'))
+    xpans = os.path.join(build_lib, cmd.distribution.get_name(), 'xpans')
+    libxpa_so = os.path.join(build_lib, file_name)
+    compile_cmd = compiler.compiler
+    compile_cmd += [xpans_o, '-o', xpans, libxpa_so]
+    compile_cmd += flags
+    print(" ".join(compile_cmd))
+    sp.check_call(compile_cmd)
+
+
 
 
 def post_install_hook(cmd):
