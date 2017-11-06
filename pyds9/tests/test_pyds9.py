94604faf91fcac323751863fe0264c53705a814d
Francesco Montesano <franz.bergesund@gmail.com>
2017-11-06 19:25:38 +0100

    open and close ds9 fixture using headless X11 (pytest-xvfb)

--- pyds9/tests/test_pyds9.py
+++ pyds9/tests/test_pyds9.py
@@ -1,3 +1,7 @@
+import random
+import subprocess as sp
+import time
+
 import numpy as np
 import pytest
 
@@ -19,6 +23,26 @@ type_mapping = parametrize('bitpix, dtype ',
                             ])
 
 
+@pytest.fixture
+def ds9_title():
+    '''Start a ds9 instance in a subprocess and returns its title'''
+    name = 'test.{}'.format(random.randint(0, 10000))
+    cmd = ['ds9', '-title', name]
+    p = sp.Popen(cmd)
+    # wait for ds9 to come alive
+    while not pyds9.ds9_targets():
+        time.sleep(0.1)
+
+    yield name
+
+    returncode = p.poll()
+    if returncode is None:
+        p.kill()
+        p.communicate()
+    elif returncode != 0:
+        raise sp.CalledProcessError(returncode, ' '.join(cmd))
+
+
 @type_mapping
 def test_bp2np(dtype, bitpix):
     """Test from bitpix to dtype"""
@@ -33,3 +57,8 @@ def test_np2bp(dtype, bitpix):
     output = pyds9._np2bp(dtype)
 
     assert output == bitpix
+
+
+def test_ds9(ds9_title):
+    ''''''
+    ds9 = pyds9.ds9_openlist(target=ds9_title)[0]
